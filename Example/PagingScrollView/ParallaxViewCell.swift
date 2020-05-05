6ed63018ddd14307205535532a51dec8edd93ceb
Seyed Samad Gholamzadeh <ssamadgh@gmail.com>
2020-05-05 16:00:59 +0430

    Some minor fixes in example

--- Example/PagingScrollView/ParallaxViewCell.swift
+++ Example/PagingScrollView/ParallaxViewCell.swift
@@ -10,11 +10,10 @@ import UIKit
 import SwiftyPagingScrollView
 
 class ParallaxViewCell: UIView {
-
-    @IBOutlet weak var parallaxImageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
-
-
+	
+	@IBOutlet weak var parallaxImageView: UIImageView!
+	@IBOutlet weak var titleLabel: UILabel!
+	
 	var entity: Character? {
 		didSet {
 			guard entity != nil else { return }
@@ -23,20 +22,20 @@ class ParallaxViewCell: UIView {
 			self.titleLabel.text = entity?.name
 		}
 	}
-		
+	
 	override func awakeFromNib() {
 		super.awakeFromNib()
 		self.layer.cornerRadius = 50
 	}
 	
 	
-
+	
 }
 
 extension ParallaxViewCell: ParallaxScrollViewDelegate {
-    
-    func didChangeParalex(to value: CGFloat) {
-        self.parallaxImageView.center.x = (self.frame.width/2)*(1 + value)
-    }
-    
+	
+	func didChangeParalex(to value: CGFloat) {
+		self.parallaxImageView.center.x = (self.frame.width/2)*(1 + value)
+	}
+	
 }
--- Example/PagingScrollView/ParallaxViewController.swift
+++ Example/PagingScrollView/ParallaxViewController.swift
@@ -10,39 +10,36 @@ import UIKit
 import SwiftyPagingScrollView
 
 class ParallaxViewController: UIViewController {
-
-	var pagingScrollView: PagingScrollView!
-
-    var list: [Character] = []
-
 	
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-		let viewSize = self.view.frame.size
+	var pagingScrollView: PagingScrollView!
+	
+	var list: [Character] = []
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
 		
-        self.view.frame.size = viewSize
-        self.pagingScrollView = PagingScrollView()
+		self.pagingScrollView = PagingScrollView()
 		let screenWidth = UIScreen.main.bounds.width
 		let screenHeihgt = UIScreen.main.bounds.height
 		
-        let pageWidth = min(screenWidth, screenHeihgt) - 100
-        let pageSize = CGSize(width: pageWidth, height: pageWidth)
-
-        self.pagingScrollView.pageSize = pageSize
-        self.pagingScrollView.pageSpace = 10
-        self.pagingScrollView.registerForPageResue(UINib(nibName: "ParallaxViewCell", bundle: nil))
-        self.pagingScrollView.pagingScrollViewDataSource = self
-        self.pagingScrollView.pagingScrollViewDelegate = self
-		self.pagingScrollView.autoresizingMask = [.flexibleTopMargin, .flexibleBottomMargin]
-        self.view.addSubview(self.pagingScrollView)
-
+		let pageWidth = min(screenWidth, screenHeihgt) - 100
+		let pageSize = CGSize(width: pageWidth, height: pageWidth)
 		
-        // Do any additional setup after loading the view.
+		self.pagingScrollView.pageSize = pageSize
+		self.pagingScrollView.pageSpace = 10
+		self.pagingScrollView.registerForPageResue(UINib(nibName: "ParallaxViewCell", bundle: nil))
+		self.pagingScrollView.pagingScrollViewDataSource = self
+		self.pagingScrollView.pagingScrollViewDelegate = self
+		self.pagingScrollView.autoresizingMask = [.flexibleTopMargin, .flexibleBottomMargin]
+		self.view.addSubview(self.pagingScrollView)
+		
+		
+		// Do any additional setup after loading the view.
 		DispatchQueue.global().async {
-
+			
 			guard let url = Bundle.main.url(forResource: "Characters", withExtension: "json") else { return }
-
+			
 			do {
 				let jsonData = try Data(contentsOf: url)
 				let list = try JSONDecoder().decode([Character].self, from: jsonData)
@@ -50,44 +47,43 @@ class ParallaxViewController: UIViewController {
 				DispatchQueue.main.async {
 					self.pagingScrollView.reloadData()
 				}
-
-
+				
 			} catch {
 				print("error is \(error)")
 			}
-
+			
 		}
 		
-    }
-    
-
+	}
+	
+	
 	override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
 		self.pagingScrollView.updatePagesLayout(animated: true)
 	}
 	
-
+	
 }
 
 extension ParallaxViewController: PagingScrollViewDataSource, PagingScrollViewDelegate {
 	
-		func numberOfPages() -> Int {
-			self.list.count
-		}
+	func numberOfPages() -> Int {
+		self.list.count
+	}
+	
+	func pagingScrollView(_ pagingScrollView: PagingScrollView, pageAt index: Int) -> UIView {
+		let page = pagingScrollView.dequeueReusablePage(for: index) as! ParallaxViewCell
 		
-		func pagingScrollView(_ pagingScrollView: PagingScrollView, pageAt index: Int) -> UIView {
-			let page = pagingScrollView.dequeueReusablePage(for: index) as! ParallaxViewCell
-			
-	        let entity = self.list[index]
-			page.entity = entity
-
-			return page
-		}
+		let entity = self.list[index]
+		page.entity = entity
 		
-		func pagingScrollView(_ pagingScrollView: PagingScrollView, didSelectPageAt index: Int) {
-			let page = pagingScrollView.pageForIndex(index) as! ParallaxViewCell
-			let entity = self.list[index]
-			print("Page did select with index \(index) and title \(entity.name)")
-		}
-
+		return page
+	}
+	
+	func pagingScrollView(_ pagingScrollView: PagingScrollView, didSelectPageAt index: Int) {
+		let page = pagingScrollView.pageForIndex(index) as! ParallaxViewCell
+		let entity = self.list[index]
+		print("Page did select with index \(index) and title \(entity.name)")
+	}
+	
 	
 }
